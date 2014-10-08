818aa3c864dd17bf2cf04c5de5c39c097df7ddba
Jackson Tian <shyvo1987@gmail.com>
2014-10-08 11:38:39 +0800

    Init documentation

--- test/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  corpid: 'wx4aa3347cec528612',
-  corpsecret: '3cHx23rTMqAgawyHLqdMrfjCXIjrjkdN_YI3iNHtAKgzNcfsDdi_3VHm4-KLJ5JI',
-  token: 'RMNlACHlV5ThzfRlVS4D4',
-  encodingAESKey: 'bAZC8S8nz5fcQ0Ui2HxQVGbC6FzyrzWKSuDbmzpBo3G'
-};
